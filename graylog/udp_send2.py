6e7a4682630367baeb393ec3f6b7be8c405fe306
unknown <Razel@Abyss.testdnssuf>
2022-03-22 13:26:59 +0300

    init

--- graylog/udp_send2.py
+++
@@ -1,9 +0,0 @@
-import socket
-from time import sleep
-
-UDP_PORT = 12201
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.connect(('localhost', UDP_PORT))
-while True:
-	sock.send(b'{ "version": "1.1", "host": "example.org", "short_message": "A short message", "level": 5, "_some_info": "foo" }')
-	sleep(1)
